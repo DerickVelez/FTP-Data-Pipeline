40803a51ce1603e5a52349e139248026790ec247
Jan Derick Velez <jderickvelez@gmail.com>
2024-05-02 14:50:32 +0800

    Added .env file

--- download_ftp_file.py
+++ download_ftp_file.py
@@ -1,32 +1,29 @@
 import ftplib
 import zipfile
+import os
 
-HOSTNAME = "localhost"
-USERNAME = "derick1"
-PASSWORD = "Workeye29"
+from dotenv import load_dotenv
 
-ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
+load_dotenv() 
+
+hostname = f"{os.getenv('DPL_HOSTNAME')}"
+username = f"{os.getenv('DPL_USERNAME')}"
+password = f"{os.getenv('DPL_PASSWORD')}"
+filename = f"{os.getenv('DPL_FILENAME')}"
+
+ftp_server = ftplib.FTP(host=hostname, user=username,passwd=password)
  
 # force UTF-8 encoding
-ftp_server.encoding = "utf-8"
+ftp_server.encoding = "utf-8"   
 
-filename = "CRED_DATA.zip"
- 
+
 # Write file in binary mode
-with open(filename, "wb") as file:
+with open(file=filename,mode="wb") as file:
     ftp_server.retrbinary(f"RETR {filename}", file.write)
  
 # Close the Connection
 ftp_server.quit()
 
 with zipfile.ZipFile(f"{filename}","r") as zip_ref:
-    zip_ref.extractall("D:\development\python\DATAPIPELINEPROJECT\extracted")
+    zip_ref.extractall("D:\\development\\python\\DATAPIPELINEPROJECT\\extracted")
 
-
-# # Enter File Name with Extension
-# filename = "gfg.txt"
- 
-# # Read file in binary mode
-# with open(filename, "rb") as file:
-#     # Command for Uploading the file "STOR filename"
-#     ftp_server.storbinary(f"STOR {filename}", file)
